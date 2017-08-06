e58b96b577ca35c793d0761614c0a7f1b3300b9a
Felix Morgner <felix.morgner@gmail.com>
2017-08-06 21:11:50 +0200

    travis: fix upload urls

--- build.py
+++ build.py
@@ -1,6 +1,11 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == '__main__':
-    builder = ConanMultiPackager(username='fmorgner', archs=['x86_64'])
+    builder = ConanMultiPackager(
+        username='fmorgner',
+        archs=['x86_64'],
+        upload='https://api.bintray.com/conan/fmorgner/conan-fmorgner',
+        remotes='https://api.bintray.com/conan/fmorgner/conan-fmorgner'
+    )
     builder.add_common_builds(shared_option_name='Botan:shared', pure_c=False)
     builder.run()
