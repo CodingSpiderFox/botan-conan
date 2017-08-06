aaa30cf3c0a3a66509478d3828eaf5ecf61221cf
Felix Morgner <felix.morgner@gmail.com>
2017-08-06 17:48:36 +0200

    conan: disable automatic x86 build in CI

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == '__main__':
-    builder = ConanMultiPackager(username='fmorgner')
+    builder = ConanMultiPackager(username='fmorgner', archs=['x86_64'])
     builder.add_common_builds(shared_option_name='Botan:shared', pure_c=False)
     builder.run()
